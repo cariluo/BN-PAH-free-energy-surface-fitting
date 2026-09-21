b994f82996d4be7c451a18b4d4c7e185b179d050
Carina Luo <107739074+cariluo@users.noreply.github.com>
2026-09-21 09:31:14 -0400

    Match fitted colorbar label styling

--- src/bn_pah_fes/plotting.py
+++ src/bn_pah_fes/plotting.py
@@ -128,7 +128,7 @@ def plot_3d_free_energy_surface(
         shrink=COLORBAR_SHRINK,
         pad=COLORBAR_PAD,
     )
-    cbar.set_label(zlabel, fontsize=20)
+    cbar.set_label(zlabel)
     plt.tight_layout()
     plt.savefig(results_dir / filename, dpi=300)
     plt.close()
